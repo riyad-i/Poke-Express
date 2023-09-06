9dc924a133975cabef9a872fb5ce594af4eb6ea0
Riyad <Riyad9876@gmail.com>
2023-09-06 10:56:16 -0400

    capitalized

--- views/Index.jsx
+++ views/Index.jsx
@@ -3,7 +3,13 @@ const React = require('react')
 
 
 function Index({pokemon}){
-    console.log(pokemon)
+    //map usually doesn't change original array, but the elements are objects so they get changed
+    pokemon.map(pokemon => {
+        let f = pokemon.name[0]
+        f = f.toUpperCase()
+        let full = f + pokemon.name.slice(1,pokemon.name.length)
+        pokemon.name = full
+    })
     return (
         <>
             <h1>Pokemon</h1>
