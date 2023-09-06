580aea76933b2a5ea39b4f98a4024610cb4d967a
Riyad <Riyad9876@gmail.com>
2023-09-06 11:20:41 -0400

    showing pictures in show

--- views/Show.jsx
+++ views/Show.jsx
@@ -3,9 +3,11 @@ const React = require('react')
 
 
 function Show({poke}){
+    console.log(poke);
     return(
         <>
         <h1>{poke.name}</h1>
+        <img src={poke.img + '.jpg'}/>
         <a href='/pokemon'>Back</a>
         </>
     )
