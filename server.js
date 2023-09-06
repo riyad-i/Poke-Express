6917d4d71c903459b831d45304382001d0c1bae6
Riyad <Riyad9876@gmail.com>
2023-09-06 10:22:58 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+
+const app = express()
+const PORT = 3000
+
+
+app.get('/', (req, res) => {
+    res.send('Working!')
+})
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`Server running on port: ${PORT}`);
+})
